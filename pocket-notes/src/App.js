b9312c712fb4457e437905b7363952df8f1e3e2f
komal <jitkarkomal158024@gmail.com>
2024-11-09 23:32:44 +0530

    pocket notes structure created

---
+++ pocket-notes/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Notes from './components/Notes';
+import Sidebar from './components/Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar/>
+      <Notes/>
+    </div>
+  );
+}
+
+export default App;
---
+++ pocket-notes/src/components/Notes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Notes() {
+  return (
+    <div className='Notes'>
+      <h1>Notes</h1>
+    </div>
+  )
+}
